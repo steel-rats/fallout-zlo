4ebbb1661ec573819b57c8cc2e2ace6904d8e47c
PsychoWalker <online-ss4@yandex.ru>
2018-01-24 00:38:51 +0300

    добавил 3-й и 4-й экраны

--- js/main.js
+++ js/main.js
@@ -69,3 +69,15 @@ $(document).ready(function () {
 function rotate() {
     $('#next').click();
 }
+
+
+$(document).ready(function(){
+    $("a[href*=#]").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
